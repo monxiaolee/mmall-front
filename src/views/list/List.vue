7e45543ee3a17cbd3d3918edd84c2be5bcdb7323
monxiaolee <li_meng_xiao@yeah.net>
2018-08-23 00:21:14 +0800

    列表页新增

---
+++ src/views/list/List.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="">
+    <nav></nav>
+    <header></header>
+</div>
+</template>
+<script>
+import Nav from '@/views/layout/Nav'
+import Header from '@/views/layout/Header'
+export default {
+    name: 'List',
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    components: {
+        Nav, 
+        Header
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
